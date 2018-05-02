0633ede941b08fa5598d3d4e013abc5f3a274fb0
Slade Utley <slade.utley@gmail.com>
2018-05-02 15:41:20 -0500

    created server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/client"));
+
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+});
