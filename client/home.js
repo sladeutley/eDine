a2897723812658bd161f0f60f70ed01b72c6777d
Slade Utley <slade.utley@gmail.com>
2018-05-03 14:27:55 -0500

    have register and login page working

---
+++ client/home.js
@@ -0,0 +1,7 @@
+document.getElementById("goToLogin").addEventListener("click", () => {
+  location.href = `${location.origin}/login`;
+});
+
+document.getElementById("goToRegister").addEventListener("click", () => {
+  location.href = `${location.origin}/register`;
+});
