06f199353937ba0dfb43d8ec09001ac07f3afd5d
Slade Utley <slade.utley@gmail.com>
2018-05-03 09:56:15 -0500

    added middleware to work with pug

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ const session = require("express-session");
 const passport = require('passport');
 const bodyParser = require('body-parser');
 
-//routes
-const routes = require('./server/routes');
+app.use(express.static(__dirname + "/client"));
+
+require('dotenv').config();
+const port = process.env.PORT || 8080
 
 //models
 // app.set("models", require("./server/models"));
 
-app.use(express.static(__dirname + "/client"));
+app.set('view engine', 'pug');
+
+//routes
+const routes = require('./server/routes');
 
 //auth
 app.use(
@@ -28,11 +33,18 @@ app.use(
 require("./server/config/passport-strat.js");
 app.use(passport.initialize());
 app.use(passport.session());
+
+//for accessing login info in pug
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+})
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(routes);
 
 //server
-app.listen(3000, () => {
-  console.log(`listening on port 3000`);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
