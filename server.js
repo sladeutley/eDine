f736f186d873ea2327fe42a22ad6a6944a3869db
Slade Utley <slade.utley@gmail.com>
2018-05-03 09:49:02 -0500

    created server and added middleware, added passport-strat.js for user login

--- server.js
+++ server.js
@@ -3,8 +3,36 @@
 const express = require("express");
 const app = express();
 
+//auth
+const session = require("express-session");
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+//routes
+const routes = require('./server/routes');
+
+//models
+// app.set("models", require("./server/models"));
+
 app.use(express.static(__dirname + "/client"));
 
+//auth
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+require("./server/config/passport-strat.js");
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
+
+//server
 app.listen(3000, () => {
   console.log(`listening on port 3000`);
 });
