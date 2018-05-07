e0caf79cca22b51ea1b5cf1bee3678f9474ca98c
Slade Utley <slade.utley@gmail.com>
2018-05-07 13:34:03 -0500

    added ability to autoincrement id on userfollow join table

--- server/models/userfollow.js
+++ server/models/userfollow.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var userFollow = sequelize.define('userFollow', {
-    id: DataTypes.INTEGER
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    }
   }, {});
   userFollow.associate = function(models) {
     // associations can be defined here
