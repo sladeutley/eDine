189c340da136c42c6f54b330d2c6b6c381945bc7
Slade Utley <slade.utley@gmail.com>
2018-05-16 11:02:31 -0500

    rendering reviews of people you follow to dom

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const googlePlacesKey = require("../googlePlacesKey").key;
+const https = require("https");
+
 module.exports.followUser = (req, res, next) => {
   // let { userFollow, User, Review } = req.app.get("models");
   let { userFollow } = req.app.get("models");
@@ -49,16 +52,73 @@ module.exports.displayPeopleUserFollowsReviews = (req, res, next) => {
   let { sequelize } = req.app.get("models");
 
   console.log("req.user.id", req.user.id);
-  sequelize.query(`
-  select * from reviews, "userFollows" 
+  sequelize
+    .query(
+      `
+  select * from reviews, users, "userFollows" 
   where reviews.user_id="userFollows"."followeeId" 
-  and "userFollows"."followerId"=${req.user.id};
-  `)
+  and "userFollows"."followerId"=${req.user.id}
+  and users.id=${req.user.id}
+  `
+    )
     .then(followeesReviews => {
-      res.json(followeesReviews[0]);
-    })
-    .catch(err => {
-      console.log("oops!", err);
-      res.status(500).json({ error: err });
+      let promiseArr = [];
+      followeesReviews[0].forEach(review => {
+        // must push each resolve (from a promise) into the promise all (promiseArr) array
+        promiseArr.push(
+          new Promise((resolve, reject) => {
+            https
+              .get(
+                `https://maps.googleapis.com/maps/api/place/details/json?placeid=${
+                  review.restaurantAPI_id
+                }&key=${googlePlacesKey}`,
+                resp => {
+                  let data = ""; // some of the data has been recieved.
+
+                  resp.on("data", chunk => {
+                    data += chunk;
+                  }); // The whole response has been received
+
+                  resp.on("end", () => {
+                    console.log("JSON.parse(data)", JSON.parse(data));
+                    // NEED 'dataValues' bc every object's properties is nested in dataValues originally. JSON.parse makes dataValues go away
+                    // res.json(JSON.parse(data));
+                    // review.dataValues.details = JSON.parse(data);
+                    review.result = JSON.parse(data);
+                    resolve(review);
+                  });
+                }
+              )
+              .on("error", err => {
+                console.log("Error: " + err.message);
+              });
+          })
+        );
+      });
+      Promise.all(promiseArr)
+        .then(data => {
+          // data.dataValues.loggedInUserId = req.user.id;
+          console.log("data", data);
+          // res.json(data);
+          console.log("req.user.id", req.user.id);
+          // res.render("welcome", { data } );
+          res.render("followees-reviews", {
+            data: data,
+            loggedInUserId: req.user.id
+          });
+        })
+        .catch(err => {
+          console.log("oopsies, something went wrong!", err);
+          res.status(500).json({ error: err });
+        });
     });
+};
+
+module.exports.displayAllUsers = (req, res, next) => {
+  let { User } = req.app.get("models");
+
+  User.findAll().then(users => {
+    // res.json(users);
+    res.render('all-users', { users })
+  });
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,10 +3,16 @@
 const { Router } = require('express');
 const router = Router();
 
-const { followUser, displayUsersPage, displayPeopleUserFollowsReviews } = require('../controllers/userCtrl');
+const { followUser, displayUsersPage, displayPeopleUserFollowsReviews, displayAllUsers } = require('../controllers/userCtrl');
 
 router.post(`/follow-user/:id`, followUser);
 router.get('/user/:id', displayUsersPage);
-router.get('/followees-reviews', displayPeopleUserFollowsReviews);
+router.get('/followees-reviews', isLoggedIn, displayPeopleUserFollowsReviews);
+router.get('/all-users', displayAllUsers)
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next(); 
+  res.redirect('/login');
+}
 
 module.exports = router;
